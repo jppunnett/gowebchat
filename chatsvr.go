4c066b23f7e8eb099bfe2526a1edf3a559042c2b
James Punnett <jppunnett@yahoo.com>
2016-05-09 22:11:25 -0400

    Pass listen and url to program

--- chatsvr.go
+++ chatsvr.go
@@ -2,65 +2,23 @@ package main
 
 // Command chatsvr starts the web-based chat server.
 //
-// Usage: chatsvr [port]
+// Usage: chatsvr [host:port] [chat URL]
 //
-// If [port] is not specified, then chatsvr defaults to 8080
+// If [host:port] is not specified, then chatsvr defaults to localhost:8083
+// If [chat URL] is not specified, then chat server defaults to host:port/chat
 import (
-	"errors"
 	"fmt"
 	"html/template"
 	"io"
-	"net"
 	"net/http"
 	"os"
 
 	"golang.org/x/net/websocket"
 )
 
-// port to listen on for incoming HTTP requests
-var port string = "8080"
-
 // Host and port on which to listen for incoming HTTP requests
-var ListenAddr string
-
-// Helper to return the "first" external IP that's up
-// See https://play.golang.org/p/BDt3qEQ_2H
-func externalIP() (string, error) {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		return "", err
-	}
-	for _, iface := range ifaces {
-		if iface.Flags&net.FlagUp == 0 {
-			continue // interface down
-		}
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue // loopback interface
-		}
-		addrs, err := iface.Addrs()
-		if err != nil {
-			return "", err
-		}
-		for _, addr := range addrs {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip == nil || ip.IsLoopback() {
-				continue
-			}
-			ip = ip.To4()
-			if ip == nil {
-				continue // not an ipv4 address
-			}
-			return ip.String(), nil
-		}
-	}
-	return "", errors.New("are you connected to the network?")
-}
+var listenAddr string = "localhost:8083"
+var chatURL string = listenAddr + "/chat"
 
 // ingoreOriginHandler is a simple interface to a WebSocket browser client.
 // It ingnores the Origin request header by default.
@@ -74,8 +32,8 @@ func (h ingoreOriginHandler) ServeHTTP(w http.ResponseWriter, req *http.Request)
 	s.ServeHTTP(w, req)
 }
 
-// Echo the data received on the WebSocket.
-func echoServer(ws *websocket.Conn) {
+// Start a chat. (For now, only echoing)
+func chatHandler(ws *websocket.Conn) {
 	io.Copy(ws, ws)
 }
 
@@ -86,30 +44,27 @@ func rootHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	err = t.Execute(w, ListenAddr)
+	err = t.Execute(w, chatURL)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func main() {
-	if len(os.Args) == 2 {
-		port = os.Args[1]
-	}
-
-	if host, err := externalIP(); err != nil {
-		panic("externalIP: " + err.Error())
-	} else {
-		ListenAddr = fmt.Sprintf("%s:%s", host, port)
-		fmt.Printf("chatsvr listening at %v\n", ListenAddr)
-
-		http.HandleFunc("/", rootHandler)
-		http.Handle("/echo", ingoreOriginHandler(echoServer))
-
-		err := http.ListenAndServe(ListenAddr, nil)
-		if err != nil {
-			panic("ListenAndServe: " + err.Error())
+	if len(os.Args) >= 2 {
+		listenAddr = os.Args[1]
+		if len(os.Args) > 2 {
+			chatURL = os.Args[2]
 		}
 	}
 
+	fmt.Printf("chatsvr listening at %v\n", listenAddr)
+
+	http.HandleFunc("/", rootHandler)
+	http.Handle("/chat", ingoreOriginHandler(chatHandler))
+
+	err := http.ListenAndServe(listenAddr, nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 }
